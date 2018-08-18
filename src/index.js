f2f051f9f5bcf21d0cb02053369110b9f4707437
Florian Keller <github@floriankeller.de>
2018-08-18 14:52:27 +0200

    Add UUIDv1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 const UUID = require('pure-uuid');
-document.getElementById('uuid').innerHTML = new UUID(4).format();
+document.getElementById('uuidv4').innerHTML = new UUID(4).format();
+document.getElementById('uuidv1').innerHTML = new UUID(1).format();

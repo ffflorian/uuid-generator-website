90649d9ab8ffcffb1d03ccddd0d7070a68689dee
Florian Keller <github@floriankeller.de>
2018-11-13 23:46:21 +0100

    feat: Add spectre.css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-const UUID = require('pure-uuid');
+import UUID from 'pure-uuid';
+
 document.getElementById('uuidv4').innerHTML = new UUID(4).format();
 document.getElementById('uuidv1').innerHTML = new UUID(1).format();
